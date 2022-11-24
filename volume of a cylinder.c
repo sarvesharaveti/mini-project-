b1c682bc0e203fcb3a0300045f1478c254e80ec3
Araveti Sarvesh <113874093+sarvesharaveti@users.noreply.github.com>
2022-11-25 01:09:45 +0530

    Update and rename to generate fibonacci triangle.c to volume of a cylinder.c

---
+++ volume of a cylinder.c
@@ -0,0 +1,12 @@
+##include <stdio.h>
+
+int main() {
+    printf("RA2211042010041\n");
+    float radius,height,volume;
+    printf("enter the radius");
+    scanf("%f",&radius);
+    printf("enter the height");
+    scanf("%f",&height);
+    printf("%f",volume=3.14*radius*radius*height);
+    return 0;
+}
