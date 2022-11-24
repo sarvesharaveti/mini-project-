c0b481d7d0b1443f95bbab8b5e0fc86210dee292
Araveti Sarvesh <113874093+sarvesharaveti@users.noreply.github.com>
2022-11-25 00:48:23 +0530

    Update and rename print the Multiplication Table of any Number.c to biggest number.c

---
+++ biggest number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+   printf("RA2211042010041\n");
+   float a,b;
+   printf("enter one number: ");
+   scanf("%f",&a);
+   printf("enter another number to compare: ");
+   scanf("%f",&b);
+   if(a>b)
+   printf("the biggest among the numbers is %f",a);
+   else
+   printf("the biggest among the number is %f",b);
+  
+    return 0;
+}
