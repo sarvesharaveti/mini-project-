a82ace1f6bcb3bf1b60434a6f9b377865f2f86ec
Araveti Sarvesh <113874093+sarvesharaveti@users.noreply.github.com>
2022-11-25 01:40:01 +0530

    Create ID card.c

---
+++ ID card.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+  printf("RA2211042010041\n");
+  // Name of Institute
+  printf("SRM institute of Science and Technology\n");
+  // Name of Student 
+  printf("Araveti Sarvesh \n");
+  // Course
+  printf("B.Tech in Computer Science and Buissness Systems\n");
+  // Regsitration Number
+  printf("RA2211042010041\n");
+  
+  return 0;
+}
