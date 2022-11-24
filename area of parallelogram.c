b00599d62f9a78c87c86887830ba160721c673e4
Araveti Sarvesh <113874093+sarvesharaveti@users.noreply.github.com>
2022-11-25 01:57:00 +0530

    Create area of parallelogram.c

---
+++ area of parallelogram.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+  printf("RA2211042010041 \n");
+  int height, base, area;
+  printf("Enter the height of the parellogram:");
+  scanf("%d", &height);
+  printf("Enter the base of the parellogram:");
+  scanf("%d", &base);
+  
+  area = height * base;
+  printf("The area of your parellogram is: %d", area);
+  
+}
