02614b369e6aba163038fa57716748e1fd75e9fa
Araveti Sarvesh <113874093+sarvesharaveti@users.noreply.github.com>
2022-11-25 00:08:53 +0530

    Update and rename Remove duplicates from the unsorted array.c to largest of 3 using pointers.c

---
+++ largest of 3 using pointers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+ int main()
+ {
+     printf("RA2211042010041\n");
+     printf("Enter marks(Math,Physics,Chemistry) : ");
+     int math, phy, chem;
+     int *ptr1 = &math;
+     int *ptr2 = &phy;
+     int *ptr3 = &chem;
+     scanf("%d %d %d",&math,&phy,&chem);
+     if(*ptr1 > *ptr2 && *ptr1 > *ptr3)
+     {
+         printf("Math\n");
+     }
+     if(*ptr2 > *ptr1 && *ptr2 > *ptr3)
+     {
+         printf("Physics\n");
+     }
+     else
+     {
+         printf("Chemistry\n");
+     }
