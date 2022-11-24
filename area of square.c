9485dd6bd0a613c78fca66a27f22271bc9d71d69
Araveti Sarvesh <113874093+sarvesharaveti@users.noreply.github.com>
2022-11-25 01:00:57 +0530

    Update and rename reverse number.c to area of square.c

---
+++ area of square.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    printf("RA2211042010041\n");
+    float side,area;
+    printf("enter the side:");
+    scanf("%f",&side);
+    printf("%f",area=side*side);
+    return 0;
+}
