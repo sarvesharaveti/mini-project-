cde872fadca5bf1d030f099009e167c93f79fe55
Araveti Sarvesh <113874093+sarvesharaveti@users.noreply.github.com>
2022-11-25 01:57:34 +0530

    Create meter to inch.c

---
+++ meter to inch.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("RA2211042010041 \n");
+  float meter, inch;
+  printf("Enter the distance in meter");
+  scanf("%f", &meter);
+  inch= meter/39.37;
+  printf("%f", inch);
+}
