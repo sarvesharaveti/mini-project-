aa4b52c24c7929da44593b274dea8ab5f50895c8
Araveti Sarvesh <113874093+sarvesharaveti@users.noreply.github.com>
2022-11-25 02:01:20 +0530

    Create product of there numbers.c

---
+++ product of there numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+  printf("RA2211042010041 \n");
+float a,b,c,product; 
+printf("enter first number:",a);
+scanf("%f",&a);
+printf("enter second number:",b);
+scanf("%f",&b);
+printf("enter third number:",c);
+scanf("%f",&c);
+product=a*b*c;
+printf("%f",product);
+return 0;
+}
