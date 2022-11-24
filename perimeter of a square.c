d23e738aa0c737215b6ffef0e962cc8e0c3d6bb4
Araveti Sarvesh <113874093+sarvesharaveti@users.noreply.github.com>
2022-11-25 01:45:12 +0530

    Create perimeter of a square.c

---
+++ perimeter of a square.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    printf("RA2211042010041 \n");
+    float side,perimeter;
+    printf("enter the side:");
+    scanf("%f",&side);
+    printf("%f",perimeter=4*side);
+    return 0;
+}
