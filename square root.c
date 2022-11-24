6cd5f4760c06a942417a65509d2429c1dc156e9f
Araveti Sarvesh <113874093+sarvesharaveti@users.noreply.github.com>
2022-11-25 01:25:17 +0530

    Create square root.c

---
+++ square root.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+    printf("RA2211042010041\n");
+    int number,root;
+    printf("enter a number:");
+    scanf("%d",&number);
+    root=sqrt(number);
+    printf("The square root of the number:%d",root);
+
+    return 0;
+}
