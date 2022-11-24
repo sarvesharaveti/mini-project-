0019c49690509174fec4ca2c871e34befc5571e0
Araveti Sarvesh <113874093+sarvesharaveti@users.noreply.github.com>
2022-11-25 02:18:12 +0530

    Create land width.c

---
+++ land width.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ int main() {
+   printf("RA2211042010041\n");
+   int a;
+   printf("Number : ");
+   scanf("%d",&a);
+   printf("%2d field width\n",a);
+   printf("%6d field width\n",a);
+   printf("%-6d with minus sign\n",a);
+   return 0;
+ }
