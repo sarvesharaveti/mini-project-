e0ae1fecaf531e6abb5c32d5d5c82430337b0574
Araveti Sarvesh <113874093+sarvesharaveti@users.noreply.github.com>
2022-11-24 23:45:54 +0530

    Update Delete an Element from Array .c

--- Delete an Element from Array .c
+++ Delete an Element from Array .c
@@ -1,36 +1,9 @@
 #include<stdio.h>
-
-int main()
-{
-    printf(RA2211042010041)\n
-    printf("\n\n\t\tStudytonight - Best place to learn\n\n\n");
-
-    int array[100], position, c, n;
-    printf("\n\nEnter number of elements in array:");
-    scanf("%d", &n);
-
-    printf("\n\nEnter %d elements\n", n);
-    for(c = 0; c < n; c++)
-        scanf("%d", &array[c]);
-
-    printf("\n\nEnter the location where you want to delete element from:  ");
-    scanf("%d", &position);
-
-    if(position >= n+1)
-        printf("\n\nDeletion not possible\n\n");
-    else 
-        // updating the locations with next elements
-        for(c = position-1; c < n-1; c++)
-        array[c] = array[c+1];
-
-    printf("\n\nResultant array is: ");
-    /* 
-        the array size gets reduced by 1 
-        after deletion of the element
-    */
-    for(c = 0; c < n-1; c++) 
-        printf("%d  ", array[c]);
-
-    printf("\n\n\t\t\tCoding is Fun !\n\n\n");
+int main(){
+    printf("RA2211042010041 \n");
+    int a,b;
+    printf("Enter a 7 digit number : ");
+    scanf("%3d%*2d%2d",&a,&b);
+    printf("The values in a and b are : %d,%d\n",a,b);
     return 0;
 }
