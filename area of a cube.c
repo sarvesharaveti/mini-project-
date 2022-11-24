1c432f01c605d32e3f249e8bf4a410a682553696
Araveti Sarvesh <113874093+sarvesharaveti@users.noreply.github.com>
2022-11-25 01:02:52 +0530

    Update and rename swap two numbers without third value.c to area of a cube.c

---
+++ area of a cube.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    printf("RA2211042010041\n");
+    int side,area;
+    printf("enter the side:");
+    scanf("%d",&side);
+    printf("%d",area=6*side*side);
+    return 0;
+}
