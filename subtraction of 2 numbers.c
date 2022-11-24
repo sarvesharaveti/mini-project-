6e559eda0d06ffbd999c5d1fd19f6d3de7f1ff86
Araveti Sarvesh <113874093+sarvesharaveti@users.noreply.github.com>
2022-11-25 01:31:23 +0530

    Create subtraction of 2 numbers.c

---
+++ subtraction of 2 numbers.c
@@ -0,0 +1,13 @@
+include<studio.h>
+int main()
+{
+    printf("RA2211042010041\n");
+    int number1,number2,result;
+    printf("enter the number1:\n");
+    scanf("%d",&number1);
+    printf("enter the number2:");
+    scanf("%d",&number2);
+    result=number2-number1;
+    printf("%d",result);
+    return 0;
+}
