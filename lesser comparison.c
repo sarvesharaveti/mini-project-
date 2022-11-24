1c468a1c539af25fe444a206e45daa718adfe22e
Araveti Sarvesh <113874093+sarvesharaveti@users.noreply.github.com>
2022-11-25 01:20:21 +0530

    Create lesser comparison.c

---
+++ lesser comparison.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  printf("RA2211042010041\n");
+  int x = 18;
+  int y = 3;
+  printf("%d", x < y); // returns 0 (false) because 18 is not less than 3
+  return 0;
+}
