963d429f732a842217983cab2228bb572878a05d
Araveti Sarvesh <113874093+sarvesharaveti@users.noreply.github.com>
2022-11-25 01:12:32 +0530

    Update and rename to perform addition and subtraction of Matrices.c to Fahrenheit to celsius.c

---
+++ Fahrenheit to celsius.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    printf("RA2211042010041\n");
+    float farenheit,celcius;
+    printf("enter the temprature in farenhiet:");
+    scanf("%f",&farenheit);
+    celcius=5.0/9.0*(farenheit-32.0);
+    printf("temprature is:%f",celcius);
+    return 0;
+}
