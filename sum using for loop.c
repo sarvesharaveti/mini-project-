62ad4e10aec14e82b1a0dde79893380f91b1c5f2
Araveti Sarvesh <113874093+sarvesharaveti@users.noreply.github.com>
2022-11-25 02:07:31 +0530

    Create sum using for loop.c

---
+++ sum using for loop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    printf("RA2211042010041\n");
+    int a, b, sum = 0;
+    printf("Enter a positive integer: ");
+    scanf("%d", &a);
+    for(b = 1; b <= a; ++b)
+    sum += b;
+    printf("Sum = %d", sum);
+
+    return 0;
+}
