e226d517537fd302508c301c789958806de38dd1
Araveti Sarvesh <113874093+sarvesharaveti@users.noreply.github.com>
2022-11-24 23:58:56 +0530

    Update and rename Matrix Multiplication.c to alphabet or not.c

---
+++ alphabet or not.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+  char W;
+  printf("RA2211042010041\n");
+  printf("Enter any character : ");
+  scanf("%c", &W);
+  if ('a' <= W && W <='z' || 'A' <= W && W<='Z')
+  {
+    printf("Given character %c is an alphabet\n", W);
+  }
+  else
+  {
+    printf("Given character %c is not an alphabet\n", W);
+  }
