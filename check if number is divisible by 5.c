296e17a3132e518fa27713707a7b177613f37eca
Araveti Sarvesh <113874093+sarvesharaveti@users.noreply.github.com>
2022-11-25 00:14:14 +0530

    Update and rename check if input Number is int or float.c to check if number is divisible by 5.c

---
+++ check if number is divisible by 5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ int main() {
+   printf("RA2211042010041\n");
+   int number;
+   printf("enter the number : ");
+   scanf("%d",&number);
+   if (number % 5 == 0 ) {
+   printf("The number is divisible by 5\n"); 
+   }
+   else {
+   printf(" The number is not divisible by 5 ");
+   }
