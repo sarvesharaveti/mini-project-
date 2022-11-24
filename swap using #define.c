61ed7e72636694acb734abf742075a4c7e1ee93e
Araveti Sarvesh <113874093+sarvesharaveti@users.noreply.github.com>
2022-11-25 02:13:17 +0530

    Create swap using #define.c

---
+++ swap using #define.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#define SWAP(a,b,temp)(temp = a , a = b, b = temp)
+int main(){
+    printf("RA2211042010041\n");
+    int x = 100 , y = 20 , temp = 0;
+    SWAP(x,y,temp);
+    printf("x = %d, y = %d\n",x,y);
+    return 0;
+}
