b43a38d6b33d062e2d0cefdaf1d2354377fefadb
Araveti Sarvesh <113874093+sarvesharaveti@users.noreply.github.com>
2022-11-25 02:14:03 +0530

    Create gets and puts.c

---
+++ gets and puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    printf("RA2211042010041\n");
+    char ch[100];
+    printf("Enter a string : ");
+    gets(ch);
+    printf("The entered string is : ");
+    puts(ch);
+    return 0;
+}
