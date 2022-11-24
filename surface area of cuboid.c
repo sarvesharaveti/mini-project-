9d2561547800c8fc7afe421e9704e8a14a2ad827
Araveti Sarvesh <113874093+sarvesharaveti@users.noreply.github.com>
2022-11-25 00:55:17 +0530

    Update and rename program to print number triangle.c to surface area of cuboid.c

---
+++ surface area of cuboid.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    printf("RA2211042010041\n");
+    int length,breadth,height,surface_area;
+    printf("enter the length:");
+    scanf("%d",&length);
+    printf("enter the breadth:");
+    scanf("%d",&breadth);
+    printf("enter the height:");
+    scanf("%d",&height);
+    printf("%d",surface_area=2*(length*breadth+length*height+breadth*height));
+    return 0;
+}
