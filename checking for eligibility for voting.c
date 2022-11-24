2f4c20c11fdd208918107da1f3971ce07cddd45a
Araveti Sarvesh <113874093+sarvesharaveti@users.noreply.github.com>
2022-11-25 01:59:47 +0530

    Create checking for eligibility for voting.c

---
+++ checking for eligibility for voting.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("RA2211042010041 \n");
+	int a ;
+	printf("Enter the age of the person: ");
+	scanf("%d",&a);
+	if (a>=18)
+	{
+		printf("Eigible for voting");
+	}
+	else
+	{
+		printf("Not eligible for voting\n");
+	}	
+
+	return 0;
+}
