6fce202c988e2daf7a870fb712adaaceb34aaac3
Araveti Sarvesh <113874093+sarvesharaveti@users.noreply.github.com>
2022-11-25 00:47:04 +0530

    Update and rename print the Largest and Smallest using Global Declaration.c to cm to meter conversion.c

---
+++ cm to meter conversion.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+ printf("RA2211042010041\n");
+    float centimetre,metre;
+    printf("enter the distance in centimetre:");
+    scanf("%f",&centimetre);
+    metre=centimetre/100;
+    printf("the distance in metre: %f",metre);
+    return 0;
+}
