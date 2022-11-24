4df5708d835aec8a692a65cc5f42060ce32506f5
Araveti Sarvesh <113874093+sarvesharaveti@users.noreply.github.com>
2022-11-25 01:40:42 +0530

    Create volume of cuboid.c

---
+++ volume of cuboid.c
@@ -0,0 +1,17 @@
+include<stdio.h>
+
+int main()
+{
+  printf("RA2211042010041\n");
+  int length, breadth, height, volume; 
+  printf("Length= ");
+  scanf("%d", &length);
+  printf("Breadth= ");
+  scanf("%d", &breadth);
+  printf("Height= ");
+  scanf("%d", &height);
+  
+  volume = length*breadth*height;
+  printf("Volume=%d", volume);
+  
+}
