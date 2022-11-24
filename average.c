8100158404767af59fb3bc9523c79398331aa066
Araveti Sarvesh <113874093+sarvesharaveti@users.noreply.github.com>
2022-11-25 01:53:46 +0530

    Create average.c

---
+++ average.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("RA2211042010041 \n");
+  int a, b, c;
+  printf("Number 1: ");
+  scanf("%d", &a);
+  
+  printf("Number 2: ");
+  scanf("%d", &b);
+  
+  printf("Number 3: ");
+  scanf("%d", &c);
+  
+  printf("Average: %f\n", (a+b+c)/3.0);
+}
