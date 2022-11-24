88a796d87fc0803facb98a3ca6299a954cf5b1fd
Araveti Sarvesh <113874093+sarvesharaveti@users.noreply.github.com>
2022-11-25 01:30:23 +0530

    Create assai.c

---
+++ assai.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ int main() {
+  printf("RA2211042010041\n");
+   char c;int i;
+   i=1;
+   c='i';
+   c='a';
+   i='c';
+   printf("The value of c is %c and value of i is %d",c,i);
+}
