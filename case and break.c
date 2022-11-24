99a4604fe83937fd2392ac8a3d511adfd40c4377
Araveti Sarvesh <113874093+sarvesharaveti@users.noreply.github.com>
2022-11-25 02:14:56 +0530

    Create case and break.c

---
+++ case and break.c
@@ -0,0 +1,16 @@
+#include <stdio.h>  
+int main()  
+{  
+    printf("RA2211042010041\n");
+    int x = 10, y = 5;   
+    switch(x>y && x+y>0)  
+    {  
+        case 1:   
+        printf("hello");  
+        break;   
+        case 0:   
+        printf("bye");  
+        break;   
+    }   
+          
+}  
