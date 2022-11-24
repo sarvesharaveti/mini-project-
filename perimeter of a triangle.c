c50ab46a2b263a47c90f6c70243262700f071ab0
Araveti Sarvesh <113874093+sarvesharaveti@users.noreply.github.com>
2022-11-25 00:53:37 +0530

    Update and rename program to print alphabet triangle.c to perimeter of a triangle.c

---
+++ perimeter of a triangle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    printf("RA2211042010041\n");
+    int base,height,perimeter;
+    printf("enter the base:");
+    scanf("%d",&base);
+    printf("enter the height:");
+    scanf("%d",&height);
+    perimeter=2*(base+height);
+    printf("%d",perimeter);
+    return 0;
+}
