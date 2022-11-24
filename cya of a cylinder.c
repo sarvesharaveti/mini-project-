6aa569c99c47b3471b489012016934ad2afa5d2f
Araveti Sarvesh <113874093+sarvesharaveti@users.noreply.github.com>
2022-11-25 00:58:20 +0530

    Update and rename remove Duplicate Element in an Array.c to cya of a cylinder.c

---
+++ cya of a cylinder.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    printf("RA2211042010041\n");
+    float radius,height,csa;
+    printf("enter the radius:");
+    scanf("%f",&radius);
+    printf("enter the height:");
+    scanf("%f",&height);
+    printf("%f",csa=2*radius*height*3.14);
+    return 0;
+}
