72b3c4b4e9033edd605a3b1f523a7945bf58a49b
Araveti Sarvesh <113874093+sarvesharaveti@users.noreply.github.com>
2022-11-25 00:45:37 +0530

    Update and rename pointer comparison for same type of pointer.c to meter to km conversion.c

---
+++ meter to km conversion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    printf("RA2211042010035 \n");
+    float number1,number2,result;
+    printf("Enter one integer:");
+    scanf("%f",&number1);
+    printf("enter second integer:");
+    scanf("%f",&number2);
+    result=number1/number2;
+    printf("%f",result);
+    return 0;
+}
