061dc893c5b511306a4873001c13e2661a56c3d2
Araveti Sarvesh <113874093+sarvesharaveti@users.noreply.github.com>
2022-11-25 00:59:32 +0530

    Update and rename reverse a String.c to tea of a cylinder.c

---
+++ tea of a cylinder.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    printf("RA2211042010041\n");
+    float radius,height,tsa;
+    printf("enter the radius:");
+    scanf("%f",&radius);
+    printf("enter the height:");
+    scanf("%f",&height);
+    printf("%f",tsa=2*3.14*radius*(height+radius));
+    return 0;
+}
